f3caf5d92f4c9c829ba0674508ec57497cf3f931
Loic Coenen <lcoenenbwu@gmail.com>
2021-12-15 15:37:42 +0100

    :sparkles: Implement registration stream and pagination

--- cypress/integration/ens-lookup/homepage.spec.js
+++ cypress/integration/ens-lookup/homepage.spec.js
@@ -4,7 +4,6 @@ const TARGET = `http://localhost:${PORT}`;
 
 const EXAMPLE_DOMAIN = "among.eth";
 const EXAMPLE_ADDRESS = "0xec886201a7bcd0d26dd036dda4fe07bb4943d1de";
-const EXAMPLE_REGISTRANT = "0xec886201a7bcd0d26dd036dda4fe07bb4943d1de";
 
 context("Homepage", () => {
   it("should allow ENS lookup", () => {
@@ -18,9 +17,43 @@ context("Homepage", () => {
 
     cy.get("[data-testid=loading]").should("be.visible");
 
-    cy.get("[data-testid=address]").contains(EXAMPLE_ADDRESS);
-    cy.get("[data-testid=registrant]").contains(EXAMPLE_REGISTRANT);
-    cy.get("[data-testid=registration-date]").should('be.visible')
-    cy.get("[data-testid=expiry-date]").should('be.visible')
+    cy.get("[data-testid=look-up] [data-testid=domain-name]").contains(EXAMPLE_DOMAIN);
+    cy.get("[data-testid=look-up] [data-testid=registrant]").contains(EXAMPLE_ADDRESS);
+    cy.get("[data-testid=look-up] [data-testid=registration-date]").should('be.visible')
+    cy.get("[data-testid=look-up] [data-testid=expiry-date]").should('be.visible')
+  });
+
+  it.only('should show history', () => {
+    cy.visit(`${TARGET}/`).wait(2000);
+
+    cy.get("[data-testid=registration-stream] [data-testid=domain-name]").should('be.visible')
+    cy.get("[data-testid=registration-stream] [data-testid=registrant]").should('be.visible')
+    cy.get("[data-testid=registration-stream] [data-testid=registration-date]").should('be.visible')
+    cy.get("[data-testid=registration-stream] [data-testid=expiry-date]").should('be.visible')
+
+	cy.get('[data-testid=current-page]').contains('Page 1').should('be.visible');
+	cy.get('[data-testid=next-page]').click();
+    cy.get("[data-testid=loading]").should("be.visible");
+	cy.get('[data-testid=current-page]').contains('Page 2').should('be.visible');
+	cy.get('[data-testid=previous-page]').click();
+	cy.get('[data-testid=current-page]').contains('Page 1').should('be.visible');
+
+  })
+
+  it("should allow reverse ENS lookup", () => {
+    cy.visit(`${TARGET}/`).wait(2000);
+    cy.get("[data-testid=query]")
+      .should("be.visible")
+      .click()
+      .type(EXAMPLE_ADDRESS);
+
+    cy.get("[data-testid=lookup-button]").should("be.visible").click();
+
+    cy.get("[data-testid=loading]").should("be.visible");
+
+    cy.get("[data-testid=look-up] [data-testid=domain-name]").contains(EXAMPLE_DOMAIN);
+    cy.get("[data-testid=look-up] [data-testid=registrant]").contains(EXAMPLE_ADDRESS);
+    cy.get("[data-testid=look-up] [data-testid=registration-date]").should('be.visible')
+    cy.get("[data-testid=look-up] [data-testid=expiry-date]").should('be.visible')
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import  format  from "date-fns/format";
+import { useState, useEffect } from "react";
+import format from "date-fns/format";
 import { useQuery, useLazyQuery, gql } from "@apollo/client";
 
 const GET_ADDRESS = gql`
@@ -17,40 +17,87 @@ const GET_ADDRESS = gql`
 `;
 
 const GET_REGISTRATION = gql`
-	query GetRegistration($id: String) {
-	  registration(id: $id ){
-		registrant {
-		  id
-		}
-		registrationDate
-		expiryDate
-		labelName
+  query GetRegistration($id: String) {
+    registration(id: $id) {
+      registrant {
+        id
+      }
+      registrationDate
+      expiryDate
+	  domain {
+		  name
 	  }
-	}
+    }
+  }
 `;
 
-const formatTimestamp = timestamp => format(new Date(parseInt(timestamp * 1000)), 'dd/MM/yyyy') 
+const N_PER_PAGE = 3;
 
-const RegistrationData = ({ id }) => {
-  const { data, loading, error } = useQuery(GET_REGISTRATION, { variables: { id } });
-	return <div>	
-	  {error ? (
-		<p>Error: {error?.message}</p>
-	  ) : loading ? (
-		<p data-testid="loading">Loading</p>
-	  ) : data ? (
-		  <>
-			<div>Registrant: <span data-testid="registrant">{data.registration.registrant.id}</span></div>
-			<div>Registration date: <span data-testid="registration-date">{formatTimestamp(data.registration.registrationDate)}</span></div>
-			<div>Expiration date: <span data-testid="expiry-date">{formatTimestamp(data.registration.expiryDate)}</span></div>
-		  </>
-	  ) : null
+const GET_REGISTRATIONS_STREAM = gql`
+  query RegistrationStream($skip: Int) {
+    registrations(first: ${N_PER_PAGE}, skip: $skip, orderBy: registrationDate, orderDirection: desc) {
+      id
+      expiryDate
+      registrationDate
+      registrant {
+        id
+      }
+	  domain {
+		  name
 	  }
-  </div>
+    }
+  }
+`;
 
-}
+const formatTimestamp = (timestamp) =>
+  format(new Date(parseInt(timestamp * 1000)), "dd/MM/yyyy");
 
-const App = () => {
+const Registration = ({
+  registrant,
+  registrationDate,
+  expiryDate,
+  domain,
+}) => (
+  <>
+    <div>
+      Domain name: <span data-testid="domain-name">{domain.name}</span>
+    </div>
+    <div>
+      Registrant: <span data-testid="registrant">{registrant.id}</span>
+    </div>
+    <div>
+      Registration date:{" "}
+      <span data-testid="registration-date">
+        {formatTimestamp(registrationDate)}
+      </span>
+    </div>
+    <div>
+      Expiration date:{" "}
+      <span data-testid="expiry-date">{formatTimestamp(expiryDate)}</span>
+    </div>
+  </>
+);
+
+const RegistrationLoader = ({ id }) => {
+  const { data, loading, error } = useQuery(GET_REGISTRATION, {
+    variables: { id },
+  });
+  const { registration } = data || {};
+
+  return (
+    <div>
+      {error ? (
+        <p>Error: {error?.message}</p>
+      ) : loading ? (
+        <p data-testid="loading">Loading</p>
+      ) : data ? (
+        <Registration {...registration} />
+      ) : null}
+    </div>
+  );
+};
+
+const LookUp = () => {
   const [query, setQuery] = useState("");
   const [resolveName, { data, loading, error }] = useLazyQuery(GET_ADDRESS);
 
@@ -59,7 +106,7 @@ const App = () => {
   };
 
   return (
-    <div>
+    <div data-testid="look-up">
       <input
         data-testid="query"
         value={query}
@@ -76,19 +123,64 @@ const App = () => {
         !data.domains.length ? (
           <p>We could not find this domain</p>
         ) : (
-			<>
-			  <p>
-				Resolved to
-				<span data-testid="address">
-				  {data.domains[0].resolvedAddress.id}
-				</span>
-			  </p>
-				<RegistrationData id={data.domains[0].labelhash}/>
-			</>
+          <>
+            <RegistrationLoader id={data.domains[0].labelhash} />
+          </>
         )
       ) : null}
     </div>
   );
 };
+
+const RegistrationStream = () => {
+  const [ page, setPage ] = useState(0);
+  const [loadRegistrationsStream, { data, loading, error }] = useLazyQuery(GET_REGISTRATIONS_STREAM);
+
+  useEffect(() => {
+	loadRegistrationsStream({ variables: { skip: page * N_PER_PAGE } });
+  }, [loadRegistrationsStream, page])
+
+  const previousPage = () => setPage(page > 0 ? page - 1 : 0)
+  const nextPage = () => setPage(page + 1)
+
+  return (
+    <div data-testid="registration-stream">
+	  <h1>Latest registration</h1>
+      {error ? (
+        <p>Error: {error?.message}</p>
+      ) : loading ? (
+        <p data-testid="loading">Loading</p>
+      ) : data ? (
+        !data.registrations.length ? (
+          <p>We could not find this domain</p>
+        ) : (
+          data.registrations.map((registration) => (
+            <Registration {...registration} />
+          ))
+        )
+      ) : null}
+	  <div data-testid="paginator">
+		<div data-testid="previous-page" onClick={previousPage}>
+			&gt; Previous 		
+		</div>
+
+		<div data-testid="current-page">
+			Page { page + 1 }  		
+		</div>
+
+		<div data-testid="next-page" onClick={nextPage}>
+			&gt; Next		
+		</div>
+	  </div>
+    </div>
+  );
+};
+
+const App = () => (
+  <>
+    <LookUp />
+    <RegistrationStream />
+  </>
+);
 
 export default App;
